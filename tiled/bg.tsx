6de25d769a1a3386b039f2a4a086a04438454b92
massie0414 <massie0414@gmail.com>
2021-09-03 06:09:01 +0900

    ファイル整理

---
+++ tiled/bg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="test" tilewidth="8" tileheight="8" tilecount="2" columns="2">
+ <image source="test.bmp" trans="00aa00" width="16" height="8"/>
+</tileset>
--- tiled/mound.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="mound" tilewidth="8" tileheight="8" tilecount="16" columns="4">
- <image source="mound.bmp" trans="00aa00" width="32" height="32"/>
-</tileset>
