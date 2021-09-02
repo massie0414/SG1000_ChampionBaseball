82dd46f6676febbb472ff0f7552aafe9c9b8e815
massie0414 <massie0414@gmail.com>
2021-09-02 10:41:00 +0900

    背景を追加

---
+++ tiled/BattersBox.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="BattersBox" tilewidth="8" tileheight="8" tilecount="768" columns="32">
+ <image source="BattersBox.bmp" trans="00aa00" width="256" height="192"/>
+</tileset>
